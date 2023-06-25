ff9ae9c1293415f608fc6478ce6a2f9ab087c267
Diana Radchuk <dupshur@bk.ru>
2023-06-25 17:45:35 +0300

    added simple service and notifications

--- app/src/main/java/com/example/learningservices/MainActivity.java
+++ app/src/main/java/com/example/learningservices/MainActivity.java
@@ -1,18 +1,31 @@
 package com.example.learningservices;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.app.LauncherActivity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
 
 import com.example.learningservices.databinding.ActivityMainBinding;
-
-import io.reactivex.rxjava3.core.Observable;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    public static final String CHANNEL_ID = "channel_id";
+    public static final String CHANNEL_NAME = "channel_name";
+
+    public static final int NOTIFICATION_PERMISSION_REQUEST_CODE = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +39,30 @@ public class MainActivity extends AppCompatActivity {
                 startService(MyService.newIntent(MainActivity.this));
             }
         });
+        binding.btnStartForegroundService.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showNotification();
+            }
+        });
+    }
+
+    private void showNotification() {
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(
+                    CHANNEL_ID,
+                    CHANNEL_NAME,
+                    NotificationManager.IMPORTANCE_DEFAULT
+            );
+            manager.createNotificationChannel(channel);
+        }
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentTitle("Title")
+                .setContentText("Some text here.............")
+                .setSmallIcon(R.drawable.ic_launcher_background)
+                .build();
+        manager.notify(1, notification);
     }
 
 }
---
+++ app/src/main/java/com/example/learningservices/MyForegroundService.java
@@ -0,0 +1,70 @@
+package com.example.learningservices;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MyForegroundService extends Service {
+
+    private CompositeDisposable compositeDisposable;
+    private static final String TAG = "MyService";
+
+    public MyForegroundService() {
+    }
+
+    //у  сервисов есть свой ЖЦ
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        compositeDisposable = new CompositeDisposable();
+        log("OnCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        log("OnStartCommand");
+        Disposable disposable = Observable.fromCallable(() -> {
+            for (int i=0; i<100; i++){
+                Thread.sleep(1000);
+                log("Timer "+i);
+            }
+            return null;
+        })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe();
+        compositeDisposable.add(disposable);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        compositeDisposable.dispose();
+        log("OnDestroy");
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    private void log(String message) {
+        Log.d(TAG, message);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, MyForegroundService.class);
+    }
+}
